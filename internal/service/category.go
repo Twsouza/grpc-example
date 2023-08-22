bf7a80057032a66906fb32e7be3aeed257a1fd47
Taynan <taynan.tws@gmail.com>
2023-08-22 11:01:53 -0300

    Create ListCategories call

--- internal/service/category.go
+++ internal/service/category.go
@@ -32,3 +32,23 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 		},
 	}, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoryList []*pb.Category
+	for _, category := range categories {
+		categoryList = append(categoryList, &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
+
+	return &pb.CategoryList{
+		Categories: categoryList,
+	}, nil
+}
